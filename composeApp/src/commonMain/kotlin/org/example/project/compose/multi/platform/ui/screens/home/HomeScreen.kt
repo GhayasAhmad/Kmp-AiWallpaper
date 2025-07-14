90097691f38aa851fb2b9eab2a287b8fef0213d7
Ghayas Ahmad <sheikhghayas47@gmail.com>
2025-07-14 10:38:17 +0500

    changes on home screen.

--- composeApp/src/commonMain/kotlin/org/example/project/compose/multi/platform/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/compose/multi/platform/ui/screens/home/HomeScreen.kt
@@ -1,15 +1,21 @@
 package org.example.project.compose.multi.platform.ui.screens.home
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
+import org.example.project.compose.multi.platform.presentation.PhotoViewModel
 import org.example.project.compose.multi.platform.ui.screens.exit.ExitScreen
+import org.koin.mp.KoinPlatform.getKoin
 
 class HomeScreen() : Screen {
+
 
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.current
+        val viewModel = remember { getKoin().get<PhotoViewModel>() }
+
         HomeScreenContent(
             onBackClick = {
                 navigator?.push(ExitScreen())
